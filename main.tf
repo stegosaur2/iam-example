d414dca8ff9082294a8a3d09eb7ef39437721b01
stegosaur2 <129029184+stegosaur2@users.noreply.github.com>
2023-03-27 00:36:18 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.4.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.33.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
